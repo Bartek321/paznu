fb73dbcfa2a597cb66d871cf2a35d51292223e6a
Krzysiek <jania.k94@gmail.com>
2020-01-19 22:51:18 +0100

    Add files via upload

--- js/mainScript.js
+++ js/mainScript.js
@@ -1,9 +1,7 @@
 document.getElementById("lightIcon").addEventListener("click", onLoginClick);
 document.getElementById("deviceCheckbox1").addEventListener("change", onCheckChange);
 document.getElementById("deviceCheckbox2").addEventListener("change", onCheckChange);
-document.getElementById("button2").addEventListener("click", onButton2Click);
-document.getElementById("button2").addEventListener("onmouseover", onButton2Click);
-document.getElementById("button3").addEventListener("click", onButton3Click);
+
 
 function onLoginClick() {
   document.getElementById("lightIcon").style.backgroundImage = 'url(img/bulbOff.png)';
@@ -34,13 +32,4 @@ function onCheckChange() {
       lightOff();
 	}	
   }
-}
-
-
-function onButton2Click() {
-  console.log(document.getElementById("1").innerText);
-  document.getElementById("button2").style.width += "10px";
-}
-function onButton3Click() {
-  document.getElementById("lightIcon").style.backgroundImage = 'url(img/bulbOn.png)';
 }
